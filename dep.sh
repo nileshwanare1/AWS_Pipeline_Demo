2ad0d0302f364d85868b1b11725e816efd030960
nileshwanare1 <nilesh_wanare@outlook.com>
2026-04-01 12:42:10 +0530

    Add script to install and configure Apache2
    This script updates package lists, installs Apache2, checks its status, and removes the existing index.html file.

---
+++ dep.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Update package lists
+echo "Updating package lists..."
+sudo apt update -y
+
+# Install Apache2
+echo "Installing Apache2 web server..."
+sudo apt install apache2 -y
+
+# Check the status of Apache2
+echo "Checking Apache2 service status..."
+sudo systemctl status apache2
+
+#Remove existing index.html file from server
+rm  /var/www/index.html
+echo "Apache installation and basic configuration complete."
+echo "You can verify the installation by navigating to your server's IP address in a web browser."
