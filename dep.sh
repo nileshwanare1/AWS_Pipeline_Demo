2b3af8df48a09c867ab7c5eba6c06bda74101aab
Nilesh <nilesh_wanare@outlook.com>
2026-04-03 14:24:25 +0530

    dep.sh file updated

--- dep.sh
+++ dep.sh
@@ -2,17 +2,17 @@
 
 # Update package lists
 echo "Updating package lists..."
-sudo apt update -y
+sudo yum update -y
 
 # Install Apache2
 echo "Installing Apache2 web server..."
-sudo apt install apache2 -y
+sudo yum install apache2 -y
 
 # Check the status of Apache2
 echo "Checking Apache2 service status..."
 sudo systemctl status apache2
 
 #Remove existing index.html file from server
-rm  /var/www/index.html
+#rm  /var/www/index.html
 echo "Apache installation and basic configuration complete."
 echo "You can verify the installation by navigating to your server's IP address in a web browser."
