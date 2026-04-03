801d1607f6c21de9c2d551e96c3022034716e629
Nilesh <nilesh_wanare@outlook.com>
2026-04-03 16:33:10 +0530

    extra files deleted

--- script.js
+++
@@ -1,9 +0,0 @@
-function showTime() {
-    const now = new Date();
-    document.getElementById("time").innerText = now.toLocaleTimeString();
-}
-
-function submitForm(event) {
-    event.preventDefault();
-    document.getElementById("msg").innerText = "Message sent successfully!";
-}
